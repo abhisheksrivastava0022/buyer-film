d09e6a31d0f5a60292108b8e6822745d17f0805c
abhishek Srivastava <abhisheksrivastava0022@gmail.com>
2024-11-17 17:00:28 +0530

    update

--- src/components/Pages/Buyer.jsx
+++ src/components/Pages/Buyer.jsx
@@ -8,6 +8,7 @@ import Footer from '../Footer/Footer';
 import ApiClient from '../API/ApiClient';
 import { Link } from 'react-router-dom';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
+import Sidebar from '../Sidebar/Sidebar';
 
 const Buyer = () => {
     const [data, setData] = useState([])
@@ -179,30 +180,8 @@ const Buyer = () => {
                     <div className="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
                         <div className="col-md-12 px-3 search-sidebar">
                             <p className="logo d-none-mobile"><img src={filmbazaar} alt="logo" /></p>
-                            <h3>Filter</h3>
-                            <form onSubmit={handleSearchform}>
-                                <div className="form-group">
-                                    <label>Title of film</label>
-                                    <div className="input-group mb-3">
-                                        <input
-                                            type="text"
-                                            className="form-control"
-                                            placeholder="Search"
-                                            aria-label="Search"
-                                            aria-describedby="basic-addon1"
-                                            name="title"
-                                            value={searchForm.title}
-                                            onChange={handleInputChange}
-                                        />
-                                    </div>
-                                </div>
+                            <Sidebar />
 
-                                <div className="form-group">
-                                    <button type="submit" className="btn btn-primary btn-yellow">
-                                        Submit
-                                    </button>
-                                </div>
-                            </form>
                         </div>
                     </div>
                 </div>
